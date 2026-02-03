33469f4afeaeece6c0152b8bd320d61b93187f68
Jacob Egner <JacobEgner@example.com>
2026-02-02 21:44:23 -0600

    some perf improvements

--- src/KdlRust/src/core/tree_search.rs
+++ src/KdlRust/src/core/tree_search.rs
@@ -59,13 +59,13 @@ where
 
         let curr_player_id = curr_state.current_player_id();
         let mut best_turn = AppraisedPlayerTurn::empty_minimum();
-        let child_states = curr_state
-            .possible_turns()
-            .into_iter()
-            .map(|turn| curr_state.after_turn(turn, true))
-            .collect::<Vec<_>>();
+        let possible_turns = curr_state.possible_turns();
 
-        for child_state in child_states {
+        for turn in possible_turns {
+            if cancellation_token.is_cancellation_requested() {
+                return best_turn;
+            }
+            let child_state = curr_state.after_turn(turn, true);
             let child_player_id = child_state.current_player_id();
             let child_turn = child_state.prev_turn();
             let mut hypo_appraised_turn = Self::find_best_turn_many_players(
@@ -92,10 +92,6 @@ where
                     }
                 }
             }
-
-            if cancellation_token.is_cancellation_requested() {
-                return best_turn;
-            }
         }
 
         best_turn
@@ -116,60 +112,95 @@ where
         }
 
         let curr_player_id = curr_state.current_player_id();
-        let mut child_states = curr_state
-            .possible_turns()
-            .into_iter()
-            .map(|turn| curr_state.after_turn(turn, true))
-            .collect::<Vec<_>>();
-
-        if analysis_level > 1 {
-            child_states.sort_by(|a, b| {
-                compare_scores(
-                    a.heuristic_score(curr_player_id),
-                    b.heuristic_score(curr_player_id),
-                    false,
-                )
-            });
-        }
+        let possible_turns = curr_state.possible_turns();
 
         let mut best_turn = AppraisedPlayerTurn::empty_minimum();
         let mut alpha = alpha;
         let beta = beta;
 
-        for child_state in child_states {
-            let child_player_id = child_state.current_player_id();
-            let child_turn = child_state.prev_turn();
-            let child_is_us = curr_player_id == child_player_id;
-            let child_alpha = if child_is_us { alpha } else { -beta };
-            let child_beta = if child_is_us { beta } else { -alpha };
-            let mut hypo_turn = Self::find_best_turn_two_players(
-                child_state,
-                analysis_level - 1,
-                cancellation_token,
-                num_states_visited,
-                child_alpha,
-                child_beta,
-            );
+        if analysis_level > 1 {
+            let mut scored_states = possible_turns
+                .into_iter()
+                .map(|turn| {
+                    let child_state = curr_state.after_turn(turn, true);
+                    let score = child_state.heuristic_score(curr_player_id);
+                    (score, child_state)
+                })
+                .collect::<Vec<_>>();
+            scored_states.sort_by(|(score_a, _), (score_b, _)| {
+                compare_scores(*score_a, *score_b, false)
+            });
+            for (_, child_state) in scored_states {
+                if cancellation_token.is_cancellation_requested() {
+                    break;
+                }
+                let child_player_id = child_state.current_player_id();
+                let child_turn = child_state.prev_turn();
+                let child_is_us = curr_player_id == child_player_id;
+                let child_alpha = if child_is_us { alpha } else { -beta };
+                let child_beta = if child_is_us { beta } else { -alpha };
+                let mut hypo_turn = Self::find_best_turn_two_players(
+                    child_state,
+                    analysis_level - 1,
+                    cancellation_token,
+                    num_states_visited,
+                    child_alpha,
+                    child_beta,
+                );
 
-            if !child_is_us {
-                hypo_turn.appraisal *= -1.0;
-            }
+                if !child_is_us {
+                    hypo_turn.appraisal *= -1.0;
+                }
 
-            if best_turn.appraisal < hypo_turn.appraisal {
-                best_turn = hypo_turn;
-                best_turn.turn = child_turn;
+                if best_turn.appraisal < hypo_turn.appraisal {
+                    best_turn = hypo_turn;
+                    best_turn.turn = child_turn;
 
-                if best_turn.appraisal > alpha {
-                    alpha = best_turn.appraisal;
+                    if best_turn.appraisal > alpha {
+                        alpha = best_turn.appraisal;
 
-                    if alpha >= beta {
-                        break;
+                        if alpha >= beta {
+                            break;
+                        }
                     }
                 }
             }
+        } else {
+            for turn in possible_turns {
+                if cancellation_token.is_cancellation_requested() {
+                    break;
+                }
+                let child_state = curr_state.after_turn(turn, true);
+                let child_player_id = child_state.current_player_id();
+                let child_turn = child_state.prev_turn();
+                let child_is_us = curr_player_id == child_player_id;
+                let child_alpha = if child_is_us { alpha } else { -beta };
+                let child_beta = if child_is_us { beta } else { -alpha };
+                let mut hypo_turn = Self::find_best_turn_two_players(
+                    child_state,
+                    analysis_level - 1,
+                    cancellation_token,
+                    num_states_visited,
+                    child_alpha,
+                    child_beta,
+                );
 
-            if cancellation_token.is_cancellation_requested() {
-                break;
+                if !child_is_us {
+                    hypo_turn.appraisal *= -1.0;
+                }
+
+                if best_turn.appraisal < hypo_turn.appraisal {
+                    best_turn = hypo_turn;
+                    best_turn.turn = child_turn;
+
+                    if best_turn.appraisal > alpha {
+                        alpha = best_turn.appraisal;
+
+                        if alpha >= beta {
+                            break;
+                        }
+                    }
+                }
             }
         }
 
